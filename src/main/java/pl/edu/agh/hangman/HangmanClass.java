0a6c6b88b72757bcc4b97fe629fbc379996ff835
KrzysztChol <ckrzysztof@student.agh.edu.pl>
2022-02-06 10:43:57 +0100

    Klasa printera hangmana

---
+++ src/main/java/pl/edu/agh/hangman/HangmanClass.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.hangman;
+
+import static pl.edu.agh.hangman.Hangman.HANGMANPICS;
+
+public class HangmanClass {
+
+    public int i = 0;
+
+    public void printHangman(){
+        System.out.println(HANGMANPICS[i]);
+        i++;
+    }
+}
